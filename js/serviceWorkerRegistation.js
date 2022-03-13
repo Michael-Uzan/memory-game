0f44502243c4b38253db78fe4327ffc375be1500
Michael Uzan <looply3@gmail.com>
2022-03-13 18:13:00 +0200

    service worker registation error

--- js/serviceWorkerRegistation.js
+++ js/serviceWorkerRegistation.js
@@ -1,7 +1,7 @@
 if ('serviceWorker' in navigator) {
     window.addEventListener('load', async () => {
         try {
-            await navigator.serviceWorker.register('/serviceWorker.js')
+            await navigator.serviceWorker.register('./serviceWorker.js')
             console.log('Service Worker: Registered')
         } catch (err) {
             console.log('Service Worker: Error: ', err)
