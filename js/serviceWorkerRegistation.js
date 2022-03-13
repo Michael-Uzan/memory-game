f552c2befe9bea6c4d038a28f9859b923fc67352
Michael Uzan <looply3@gmail.com>
2022-03-13 16:53:56 +0200

    add service worker to work offline

---
+++ js/serviceWorkerRegistation.js
@@ -0,0 +1,10 @@
+if ('serviceWorker' in navigator) {
+    window.addEventListener('load', async () => {
+        try {
+            await navigator.serviceWorker.register('../serviceWorker.js')
+            console.log('Service Worker: Registered')
+        } catch (err) {
+            console.log('Service Worker: Error: ', err)
+        }
+    })
+}
